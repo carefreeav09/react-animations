57e996242fa553b9ece57e66319832b27678597d
Aabhushan Gautam <carefreeav09@gmail.com>
2019-12-11 11:53:53 +0545

    animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import {TimelineMax, Power4} from "gsap";
 import './App.css';
+import styled from "styled-components";
 
 function App() {
+
+
+    const handleAnimationOnClick = () => {
+        const timeline = new TimelineMax();
+        let c = '#expandButton';
+        let d = '#showDiv';
+        timeline.from(`${c}`, 0, { y: 0})
+            .to(`${c}`, 1, { width: '300px', ease: Power4.easeIn})
+            .to(`${d}`, 0, {display: 'block'})
+            .to(`${d}`, 2, {opacity: 1, ease: Power4.easeInOut}, '-=0.5')
+            .to(`${d}`, 2 , {height: '400px', ease: Power4.easeInOut, delay: '-1'})
+    };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className={'btn btn-success'} id='expandButton' onClick={handleAnimationOnClick} > Expand </button>
+
+          <div id={'showDiv'}  className="card mt-5" style={{
+              display: "none",
+              opacity: 0
+          }}>
+                  <div className="card-body">
+                      <h1 className={'text-primary'}>Show me later</h1>
+                  </div>
+          </div>
       </header>
     </div>
   );
